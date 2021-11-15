3607795bbb9a7e0a7153b2c5094f9c6872f55a99
TheLeftTomb707 <50126797+TheLeftTomb707@users.noreply.github.com>
2021-11-15 15:15:01 -0700

    Add files via upload

---
+++ Quiz/script.js
@@ -0,0 +1,65 @@
+function verify() {
+    if (document.getElementById("family1").checked) {
+        document.getElementById("familys1").style.backgroundColor = "green";
+    }
+    if (document.getElementById("family2").checked) {
+        document.getElementById("familys2").style.backgroundColor = "red";
+    }
+    if (document.getElementById("family3").checked) {
+        document.getElementById("familys3").style.backgroundColor = "red";
+    }
+    if (document.getElementById("family4").checked) {
+        document.getElementById("familys4").style.backgroundColor = "green";
+    }
+    if (document.getElementById("family5").checked) {
+        document.getElementById("familys5").style.backgroundColor = "green";
+    }
+    if (document.getElementById("Metro1").checked) {
+        document.getElementById("Metros1").style.backgroundColor = "red";
+    }
+    if (document.getElementById("Metro2").checked) {
+        document.getElementById("Metros2").style.backgroundColor = "red";
+    }
+    if (document.getElementById("Metro3").checked) {
+        document.getElementById("Metros3").style.backgroundColor = "red";
+    }
+    if (document.getElementById("Metro4").checked) {
+        document.getElementById("Metros4").style.backgroundColor = "green";
+    }
+    if (document.getElementById("storage1").checked) {
+        document.getElementById("storages1").style.backgroundColor = "red";
+    }
+    if (document.getElementById("storage2").checked) {
+        document.getElementById("storages2").style.backgroundColor = "red";
+    }
+    if (document.getElementById("storage3").checked) {
+        document.getElementById("storages3").style.backgroundColor = "green";
+    }
+    if (document.getElementById("storage4").checked) {
+        document.getElementById("storages4").style.backgroundColor = "red";
+    }
+    if (document.getElementById("package1").checked) {
+        document.getElementById("packages1").style.backgroundColor = "red";
+    }
+    if (document.getElementById("package2").checked) {
+        document.getElementById("packages2").style.backgroundColor = "green";
+    }
+    if (document.getElementById("package3").checked) {
+        document.getElementById("packages3").style.backgroundColor = "red";
+    }
+    if (document.getElementById("package4").checked) {
+        document.getElementById("packages4").style.backgroundColor = "red";
+    }
+    if (document.getElementById("state1").checked) {
+        document.getElementById("states1").style.backgroundColor = "red";
+    }
+    if (document.getElementById("state2").checked) {
+        document.getElementById("states2").style.backgroundColor = "red";
+    }
+    if (document.getElementById("state3").checked) {
+        document.getElementById("states3").style.backgroundColor = "red";
+    }
+    if (document.getElementById("state4").checked) {
+        document.getElementById("states4").style.backgroundColor = "green";
+    }
+}
